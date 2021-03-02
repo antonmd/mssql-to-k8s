52994d57148500536162c9493b26a7ee61da76fb
Anton Dvornikov <antonpostrest@gmail.com>
2021-03-02 13:28:17 +0100

    Added jenkins test file

--- db1/db-init.sh
+++ db1/db-init.sh
@@ -5,6 +5,6 @@ mkdir /var/opt/mssql/ReplData/
 chown mssql /var/opt/mssql/ReplData/
 chgrp mssql /var/opt/mssql/ReplData/
 
-echo "running set up script"
+echo "Running set up script"
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i /tmp/db-init.sql
